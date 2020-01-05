6270e961c1c76fbfe75de65c60b676d63eeeac91
Jay Rodrigo <>
2020-01-05 21:05:15 +0530

    Commit Project

---
+++ src/common/PredefineMethods.java
@@ -0,0 +1,197 @@
+package common;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+import com.toedter.calendar.JDateChooser;
+import database.DBconnect;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.*; 
+//import org.freixas.jcalendar.JCalendarCombo;
+import java.util.*;
+import static javax.swing.JOptionPane.showConfirmDialog;
+import net.proteanit.sql.DbUtils;
+
+public class PredefineMethods {
+
+
+    private String adminPassword = "12345";
+    
+    //Get System date function
+    public static String getSystemDate(){
+        return new SimpleDateFormat("EEE, MMM d, yyyy")
+                .format(Calendar.getInstance().getTime());
+    }
+    
+    //Get System Time Function
+    public static String getSystemTime(){
+        return new SimpleDateFormat("h:mm a")
+                .format(Calendar.getInstance().getTime());
+    }
+    
+    //To Set Panel Color Function
+    public static void setColor(JPanel panel) { panel.setBackground(new Color(0, 165, 165));}
+    
+    //To Reset Panel Color Function
+    public static void resetColor(JPanel panel) { panel.setBackground(new Color(0, 150, 150));}
+    
+    //To Set Button Color function 
+    public static void setButtonColor(JButton button) { button.setBackground(new Color(0, 165, 165));}
+    
+    //To reset Button Color function 
+    public static void resetButtonColor(JButton button) { button.setBackground(new Color(0, 150, 150));}
+    
+        //To Set Label Color function 
+    public static void setLabelColor(JLabel label) { label.setBackground(new Color(0, 165, 165));}
+    
+    //To reset Button Color function 
+    public static void resetlabelnColor(JLabel label) { label.setBackground(new Color(0, 150, 150));}
+
+    //Return admin Password
+    public String getAdminPassword() {return adminPassword;}
+    
+    //new JoptionPane
+    public static void viewJoptionPane(String message){
+        JOptionPane.showMessageDialog(new JFrame(),message,"Alert",JOptionPane.WARNING_MESSAGE);    }
+    
+    //new JoptionPane Dialog Box
+    public static int viewJoptionPaneConfirm(String message){
+        return JOptionPane.showConfirmDialog(new JFrame(),message,"Alert",JOptionPane.WARNING_MESSAGE); 
+    }
+    
+    //change database values (insert, update, delete)
+    public static void editDB(String qry){
+        try{ DBconnect.connect().prepareStatement(qry).execute();
+        }catch(Exception e){e.printStackTrace();}}
+    
+    //view database values and return Resultset
+    public static ResultSet viewDBresultset(String qry){
+        ResultSet rs = null;
+        try{ rs =  DBconnect.connect().prepareStatement(qry).executeQuery();
+        }catch(Exception e){e.printStackTrace();}
+           return rs;
+    }
+    
+    //get single value from DB 
+    public static String viewDBValue(String qry, String retrieveValue) throws SQLException{
+        ResultSet rs = viewDBresultset(qry);
+        String value= null;
+        while(rs.next()){value = rs.getString(retrieveValue);}
+        return value;
+    }
+    
+    //get TextField Values to string
+    public static String getTextField(JTextField text){return text.getText().toString();}
+    
+    //get Selected ComboBoxValue 
+    public static String getSelectedCombo(JComboBox box){return box.getSelectedItem().toString();}
+    
+        //get Selected ComboBoxValue 
+//    public static String getSelectedDate(JCalendarCombo box){return box.getSelectedItem().toString();}
+        public static String getSelectedDate(JDateChooser box) {
+        return box.getDate().toString();
+        }
+    
+    //get TextLabel Values to string
+    public static String getLabel(JLabel text){return text.getText().toString();}
+    
+    //get Button Values to string
+    public static String getButton(JButton text){return text.getText().toString();}
+    
+    //set textFieldValue
+    public static void setTextField(JTextField text, String value){text.setText(value); }
+    
+    //set comboBox Value
+    public static void setComboBox(JComboBox box, String value){box.setSelectedItem(value); }
+    
+    //Fill combox with database values
+    public static void fillComboBox(JComboBox box, String qry) throws SQLException{
+        ResultSet rs = viewDBresultset(qry);
+         while (rs.next()) {box.addItem(rs.getString(1));}
+    }
+    
+    //to write String in a file
+    public static void writeInFile (String filePath, String text) throws IOException{
+        FileOutputStream outputStream = new FileOutputStream(filePath);
+        byte[] strToBytes = text.getBytes();
+        outputStream.write(strToBytes);
+        outputStream.close();
+    }
+    
+    //Read file to srting 
+    public static String readFile (String filePath) throws IOException{
+        File file = new File(filePath);
+        StringBuilder fileContents = new StringBuilder((int)file.length());    
+        try (Scanner scanner = new Scanner(file)) {
+        while(scanner.hasNextLine()) {
+            fileContents.append(scanner.nextLine() + System.lineSeparator());}
+      } String result = "";
+        for(int x = 0 ; x < fileContents.toString().length() ; x++){
+            if(Character.isLetter(fileContents.toString().charAt(x))){
+                result = result + fileContents.toString().charAt(x);}
+        } return result;
+    }
+    
+    //tableload 
+    public static void tableload(String qry, JTable table){
+        try{ table.setModel(DbUtils.resultSetToTableModel(viewDBresultset(qry)));
+        }catch(Exception e){e.printStackTrace();}
+    }
+    
+    //Get update value
+    public static String getUpdateValue (String msg, String actualValue){
+        String input = JOptionPane.showInputDialog(null,msg);
+        if(input == (null) ){
+            input = actualValue;
+        }
+        return input;
+    }
+    
+    //Print table to PDF
+    public void printPDF(String filePath, JTable table) throws Exception{
+        try{
+        Document doc = new Document(PageSize.A4.rotate() );
+        PdfWriter.getInstance(doc, new FileOutputStream(filePath));
+        doc.open();
+        PdfPTable pdfTable = new PdfPTable(table.getColumnCount());
+            for (int i = 0; i < table.getColumnCount(); i++) {
+                pdfTable.addCell(table.getColumnName(i));
+            } for (int rows = 0; rows < table.getRowCount(); rows++) {
+                for (int cols = 0; cols < table.getColumnCount(); cols++) {
+                    pdfTable.addCell(table.getModel().getValueAt(rows, cols).toString());
+                }
+            }
+            doc.add(pdfTable);
+            doc.close();
+            viewJoptionPane("PDF Generated");
+            } catch (DocumentException ex) {
+            ex.printStackTrace();
+            } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            }
+    }
+    
+        //generate 4digit number 
+    public String generate4Digits(){
+        return String.format("%04d", new Random().nextInt(10000));
+    }
+    
+
+   
+   
+}
---
+++ src/maintenance/AddVehicle.java
@@ -0,0 +1,374 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maintenance;
+
+import common.PredefineMethods;
+
+/**
+ *
+ * @author User
+ */
+public class AddVehicle extends javax.swing.JFrame {
+
+    /**
+     * Creates new form AddVehicle
+     */
+    public AddVehicle() {
+        initComponents();
+    }
+
+    private void setAllNull() {
+        PredefineMethods.setTextField(veno, "");
+        PredefineMethods.setTextField(mod, "");
+        PredefineMethods.setComboBox(typev, "Select..");
+        PredefineMethods.setComboBox(col, "Select..");
+        PredefineMethods.setTextField(cumil, "");
+        PredefineMethods.setTextField(yr, "");
+        PredefineMethods.setTextField(delname, "");
+        PredefineMethods.setComboBox(statv, "Select..");
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        statv = new javax.swing.JComboBox<>();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jSeparator3 = new javax.swing.JSeparator();
+        typev = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        veno = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        mod = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        cumil = new javax.swing.JTextField();
+        col = new javax.swing.JComboBox<>();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        yr = new javax.swing.JTextField();
+        jLabel11 = new javax.swing.JLabel();
+        delname = new javax.swing.JTextField();
+        btnEdit = new javax.swing.JButton();
+        error = new javax.swing.JLabel();
+        vehicleDOP = new com.toedter.calendar.JDateChooser();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel1.setText("Add Vechile");
+
+        jLabel3.setText("Vechile  No");
+
+        statv.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Select..", "Brand New", "Second Owner", "Rented", "" }));
+
+        jLabel6.setText("Type");
+
+        jLabel7.setText("Status");
+
+        jSeparator3.setBackground(new java.awt.Color(204, 204, 204));
+
+        typev.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Select..", "car", "van", "lorry", "bike", "tractors", "container truck" }));
+
+        jLabel2.setText("Date of purchase");
+
+        veno.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                venoFocusLost(evt);
+            }
+        });
+        veno.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                venoActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setText("Model");
+
+        mod.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                modActionPerformed(evt);
+            }
+        });
+
+        jLabel8.setText(" Colour");
+
+        cumil.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cumilActionPerformed(evt);
+            }
+        });
+        cumil.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                cumilKeyTyped(evt);
+            }
+        });
+
+        col.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Select..", "white", "Black", "sky blue", "red", "grey", "orange" }));
+
+        jLabel9.setText("Current Milage");
+
+        jLabel10.setText("Year");
+
+        yr.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                yrActionPerformed(evt);
+            }
+        });
+        yr.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                yrKeyTyped(evt);
+            }
+        });
+
+        jLabel11.setText("Dealer Name");
+
+        btnEdit.setBackground(new java.awt.Color(0, 0, 255));
+        btnEdit.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        btnEdit.setForeground(new java.awt.Color(255, 255, 255));
+        btnEdit.setText("Add Vehicle");
+        btnEdit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 372, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 490, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(30, 30, 30)
+                        .addComponent(veno, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(jLabel5)
+                        .addGap(76, 76, 76)
+                        .addComponent(mod, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(40, 40, 40)
+                        .addComponent(typev, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(jLabel8)
+                        .addGap(69, 69, 69)
+                        .addComponent(col, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(jLabel9)
+                        .addGap(26, 26, 26)
+                        .addComponent(cumil, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(jLabel11)
+                        .addGap(36, 36, 36)
+                        .addComponent(delname, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(60, 60, 60)
+                        .addComponent(statv, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(error, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(50, 50, 50)
+                        .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addGap(27, 27, 27)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(yr, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(vehicleDOP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGap(14, 14, 14)))))
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 2, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(8, 8, 8)
+                .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 13, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(7, 7, 7)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(veno, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(5, 5, 5)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jLabel5))
+                    .addComponent(mod, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(typev, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(5, 5, 5)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jLabel8))
+                    .addComponent(col, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(14, 14, 14)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jLabel9))
+                    .addComponent(cumil, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel2)
+                    .addComponent(vehicleDOP, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(14, 14, 14)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(yr, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel11)
+                    .addComponent(delname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(8, 8, 8)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(statv, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(9, 9, 9)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(error, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(6, 6, 6)
+                        .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(0, 2, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void venoFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_venoFocusLost
+ 
+    }//GEN-LAST:event_venoFocusLost
+
+    private void venoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_venoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_venoActionPerformed
+
+    private void modActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_modActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_modActionPerformed
+
+    private void cumilActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cumilActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cumilActionPerformed
+
+    private void cumilKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_cumilKeyTyped
+       
+    }//GEN-LAST:event_cumilKeyTyped
+
+    private void yrActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_yrActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_yrActionPerformed
+
+    private void yrKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_yrKeyTyped
+    
+    }//GEN-LAST:event_yrKeyTyped
+
+    private void btnEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditActionPerformed
+            String qry = "INSERT INTO vehicle (vehicle_no, model, type, colour, mileage, purchase_date, year, dealer, status) VALUES "
+                    + "('"+PredefineMethods.getTextField(veno)+"',"
+                    + "'"+PredefineMethods.getTextField(mod)+"',"
+                    + "'"+PredefineMethods.getSelectedCombo(typev)+"',"
+                    + "'"+PredefineMethods.getSelectedCombo(col)+"',"
+                    + "'"+PredefineMethods.getTextField(cumil)+"',"
+                    + "'"+PredefineMethods.getSelectedDate(vehicleDOP)+"',"
+                    + "'"+PredefineMethods.getTextField(yr)+"',"
+                    + "'"+PredefineMethods.getTextField(delname)+"',"
+                    + "'"+PredefineMethods.getSelectedCombo(statv)+"')";
+            
+            PredefineMethods.editDB(qry);
+            PredefineMethods.viewJoptionPane("Customer Successfully Added");
+       
+            setAllNull();
+    }//GEN-LAST:event_btnEditActionPerformed
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ViewTools.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ViewTools.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ViewTools.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ViewTools.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ViewTools().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton btnEdit;
+    private javax.swing.JComboBox<String> col;
+    private javax.swing.JTextField cumil;
+    private javax.swing.JTextField delname;
+    private javax.swing.JLabel error;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JTextField mod;
+    private javax.swing.JComboBox<String> statv;
+    private javax.swing.JComboBox<String> typev;
+    private com.toedter.calendar.JDateChooser vehicleDOP;
+    private javax.swing.JTextField veno;
+    private javax.swing.JTextField yr;
+    // End of variables declaration//GEN-END:variables
+
+    
+}
